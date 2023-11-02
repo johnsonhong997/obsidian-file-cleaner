69f2d8a788ce66071952f1afcb38e56f25348861
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-11-02 20:28:47 +0100

    replaced let with const where applicable

--- src/util.ts
+++ src/util.ts
@@ -4,11 +4,11 @@ import { t } from "./translations/helper";
 
 export const cleanFiles = async (app: App, setting: FileCleanerSettings) => {
   // 获取空白Markdown文件
-  let mdFiles = app.vault.getMarkdownFiles();
-  let emptyMdFiles: TFile[] = [];
-  let emptyRegex = /\S/;
-  for (let file of mdFiles) {
-    let content = await app.vault.cachedRead(file);
+  const mdFiles = app.vault.getMarkdownFiles();
+  const emptyMdFiles: TFile[] = [];
+  const emptyRegex = /\S/;
+  for (const file of mdFiles) {
+    const content = await app.vault.cachedRead(file);
     if (file.stat.size === 0) {
       emptyMdFiles.push(file);
     } else if (!emptyRegex.test(content)) {
@@ -17,46 +17,46 @@ export const cleanFiles = async (app: App, setting: FileCleanerSettings) => {
   }
 
   // 获取未使用附件
-  let files = app.vault.getFiles();
+  const files = app.vault.getFiles();
   const attachmentRegex = /(.jpg|.jpeg|.png|.gif|.svg|.pdf)$/i;
-  let attachments: TFile[] = [];
-  for (let file of files) {
+  const attachments: TFile[] = [];
+  for (const file of files) {
     if (file.name.match(attachmentRegex)) {
       attachments.push(file);
     }
   }
 
-  let usedAttachments: any = [];
-  let resolvedLinks = app.metadataCache.resolvedLinks;
+  const usedAttachments: any = [];
+  const resolvedLinks = app.metadataCache.resolvedLinks;
   if (resolvedLinks) {
     for (const [mdFile, links] of Object.entries(resolvedLinks)) {
       for (const [path, times] of Object.entries(resolvedLinks[mdFile])) {
-        let attachmentMatch = path.match(attachmentRegex);
+        const attachmentMatch = path.match(attachmentRegex);
         if (attachmentMatch) {
-          let file = app.vault.getAbstractFileByPath(path);
+          const file = app.vault.getAbstractFileByPath(path);
           usedAttachments.push(file);
         }
       }
     }
   }
 
-  let unusedAttachments = attachments.filter(
+  const unusedAttachments = attachments.filter(
     (file) => !usedAttachments.includes(file),
   );
 
   // 获取排除文件
-  let excludedFiles: TFile[] = [];
+  const excludedFiles: TFile[] = [];
   let cleanFiles = emptyMdFiles.concat(unusedAttachments);
-  let excludedFolders = setting.excluded;
-  let excludedFoldersCleaned = new Set(
+  const excludedFolders = setting.excluded;
+  const excludedFoldersCleaned = new Set(
     excludedFolders.split(/\n/).map((folderPath) => {
       return folderPath.trim();
     }),
   );
   excludedFoldersCleaned.delete("");
-  for (let excludedFolder of excludedFoldersCleaned) {
-    let pathRegex = new RegExp("^" + excludedFolder + "/");
-    for (let file of cleanFiles) {
+  for (const excludedFolder of excludedFoldersCleaned) {
+    const pathRegex = new RegExp("^" + excludedFolder + "/");
+    for (const file of cleanFiles) {
       if (pathRegex.test(file.path)) {
         excludedFiles.push(file);
       }
@@ -67,10 +67,10 @@ export const cleanFiles = async (app: App, setting: FileCleanerSettings) => {
   cleanFiles = cleanFiles
     .concat(excludedFiles)
     .filter((v) => !cleanFiles.includes(v) || !excludedFiles.includes(v));
-  let len = cleanFiles.length;
+  const len = cleanFiles.length;
   if (len > 0) {
-    let destination = setting.destination;
-    for (let file of cleanFiles) {
+    const destination = setting.destination;
+    for (const file of cleanFiles) {
       console.log(file.name + " cleaned");
       if (destination === "permanent") {
         await app.vault.delete(file);
