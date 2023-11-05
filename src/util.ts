80433c3c7c88fb3b83dbcabdea17f99f727739fe
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-11-05 16:52:23 +0100

    simplified cleanup function

--- src/util.ts
+++ src/util.ts
@@ -21,79 +21,52 @@ async function removeFile(
   }
 }
 
-export const cleanFiles = async (app: App, setting: FileCleanerSettings) => {
-  // 获取空白Markdown文件
-  const mdFiles = app.vault.getMarkdownFiles();
-  const emptyMdFiles: TFile[] = [];
-  const emptyRegex = /\S/;
-  for (const file of mdFiles) {
-    const content = await app.vault.cachedRead(file);
-    if (file.stat.size === 0) {
-      emptyMdFiles.push(file);
-    } else if (!emptyRegex.test(content)) {
-      emptyMdFiles.push(file);
-    }
-  }
-
-  // 获取未使用附件
-  const files = app.vault.getFiles();
-  const attachmentRegex = /(.jpg|.jpeg|.png|.gif|.svg|.pdf)$/i;
-  const attachments: TFile[] = [];
-  for (const file of files) {
-    if (file.name.match(attachmentRegex)) {
-      attachments.push(file);
-    }
-  }
-
-  const usedAttachments: any = [];
-  const resolvedLinks = app.metadataCache.resolvedLinks;
-  if (resolvedLinks) {
-    for (const [mdFile, links] of Object.entries(resolvedLinks)) {
-      for (const [path, times] of Object.entries(resolvedLinks[mdFile])) {
-        const attachmentMatch = path.match(attachmentRegex);
-        if (attachmentMatch) {
-          const file = app.vault.getAbstractFileByPath(path);
-          usedAttachments.push(file);
-        }
-      }
-    }
-  }
-
-  const unusedAttachments = attachments.filter(
-    (file) => !usedAttachments.includes(file),
+export async function cleanFiles(app: App, settings: FileCleanerSettings) {
+  const excludedFoldersRegex = RegExp(`^${settings.excludedFolders.join("|")}`);
+  const allowedExtensions = RegExp(
+    `md|${settings.attachmentExtensions.join("|")}`,
   );
+  const inUseAttachments = Object.entries(app.metadataCache.resolvedLinks)
+    .map(([parent, child]) => Object.keys(child))
+    .filter((file) => file.length > 0)
+    .map((file) => file.pop())
+    .reduce(
+      (prev, cur) => (!prev.includes(cur) ? [...prev, cur] : [...prev]),
+      [],
+    );
 
-  // 获取排除文件
-  const excludedFiles: TFile[] = [];
-  let cleanFiles = emptyMdFiles.concat(unusedAttachments);
-  const excludedFolders = setting.excludedFolders;
-  const excludedFoldersCleaned = new Set(
-    excludedFolders.map((folderPath) => {
-      return folderPath.trim();
-    }),
-  );
-  excludedFoldersCleaned.delete("");
-  for (const excludedFolder of excludedFoldersCleaned) {
-    const pathRegex = new RegExp("^" + excludedFolder + "/");
-    for (const file of cleanFiles) {
-      if (pathRegex.test(file.path)) {
-        excludedFiles.push(file);
-      }
-    }
-  }
+  // Get list of all files
+  const files: TFile[] = app.vault
+    .getFiles()
+    .filter((file) =>
+      // Filter out files from excluded folders
+      settings.excludedFolders.length > 0
+        ? !file.path.match(excludedFoldersRegex)
+        : file,
+    )
+    .filter((file) =>
+      // Filters out only allowed extensions (including markdowns)
+      file.extension.match(allowedExtensions),
+    )
+    .filter(
+      (file) =>
+        // Filters out any markdown file that is empty
+        (file.extension === "md" && file.stat.size === 0) ||
+        file.extension !== "md",
+    )
+    .filter(
+      (file) =>
+        // Filters any attachment that is not in use
+        !inUseAttachments.includes(file.path) && file,
+    );
 
-  // 执行清理
-  cleanFiles = cleanFiles
-    .concat(excludedFiles)
-    .filter((v) => !cleanFiles.includes(v) || !excludedFiles.includes(v));
-  const len = cleanFiles.length;
-  if (len > 0) {
-    for (const file of cleanFiles) {
-      console.log(file.name + " cleaned");
-      removeFile(file, app, setting);
+  // Run cleanup
+  if (files.length > 0) {
+    for (const file of files) {
+      removeFile(file, app, settings);
     }
     new Notice(translate().Notifications.CleanSuccessful);
   } else {
     new Notice(translate().Notifications.NoFileToClean);
   }
-};
+}
